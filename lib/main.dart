36d88edc256d3385d273d3c83b595768ecfc8a1b
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-08-13 23:04:13 +0530

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,47 +6,20 @@ void main() {
 
 class Application extends StatefulWidget {
   @override
-  _ApplicationState createState() => _ApplicationState();
+  State<Application> createState() => new _ApplicationState();
 }
 
 class _ApplicationState extends State<Application> {
-  List<int> _items = [];
-
-  @override
-  void initState() {
-    super.initState();
-    for (int i = 0; i < 50; i++) {
-      _items.add(i);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "New Test App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Grid"),
-        ),
-        body: GridView.builder(
-          itemCount: _items.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              color: Colors.blue,
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Center(
-                  child: Text(
-                    _items[index].toString(),
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-            );
-          },
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.green,
+          title: new Text("AppBar Widget"),
+          titleSpacing: 100.0,
+          elevation: 30.0,
+          toolbarOpacity: 0.5,
         ),
       ),
     );
