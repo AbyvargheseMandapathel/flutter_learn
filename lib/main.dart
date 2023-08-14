4152d568e82e7ab804fb86c617c3b8dc4a247570
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-08-14 16:30:14 +0530

    appbar_action

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,43 @@ void main() {
 }
 
 class Application extends StatefulWidget {
+  const Application({Key? key}) : super(key: key);
+
   @override
-  State<Application> createState() => new _ApplicationState();
+  State<Application> createState() => _ApplicationState();
 }
 
 class _ApplicationState extends State<Application> {
+  String data = "";
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          backgroundColor: Colors.green,
-          title: new Text("AppBar Widget"),
-          titleSpacing: 100.0,
-          elevation: 30.0,
-          toolbarOpacity: 0.5,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("App Bar Widget"),
+          leading: Icon(Icons.menu),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  data = "This is Arrow Forward Icon";
+                });
+              },
+              icon: Icon(Icons.arrow_forward),
+            ),
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  data = "This is Close Icon";
+                });
+              },
+              icon: Icon(Icons.close),
+            ),
+          ],
+        ),
+        body: Center(
+          child: Text(data),
         ),
       ),
     );
